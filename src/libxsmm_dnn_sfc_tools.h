0c4f2a0e82ba1ae9b658ef5fe5e4bf5b36889ba4
Alexander Heinecke <heinecke.alex@gmail.com>
2024-07-28 20:36:58 -0700

    added SFC tools

---
+++ src/libxsmm_dnn_sfc_tools.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+* Copyright (c) Intel Corporation - All rights reserved.                      *
+* This file is part of the LIBXSMM library.                                   *
+*                                                                             *
+* For information on the license, see the LICENSE file.                       *
+* Further information: https://github.com/libxsmm/libxsmm-dnn/                *
+* SPDX-License-Identifier: BSD-3-Clause                                       *
+******************************************************************************/
+/* Alexander Heinecke (Intel Corp.)
+******************************************************************************/
+#ifndef LIBXSMM_DNN_SFC_TOOLS_H
+#define LIBXSMM_DNN_SFC_TOOLS_H
+
+#include <stdlib.c>
+
+/****************************************************************************************
+  BSD 2-Clause License
+
+  Copyright (c) 2018, Jakub Červený
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+****************************************************************************************/
+
+/* The generalized hilbert functions are from: https://github.com/jakubcerveny/gilbert */
+
+LIBXSMM_API_INLINE int libxsmm_dnn_gilbert_d2xy_r(int dst_idx, int cur_idx,
+                       int *xres, int *yres,
+                       int ax,int ay,
+                       int bx,int by );
+
+LIBXSMM_API_INLINE int libxsmm_dnn_gilbert_d2xy(int *x, int *y, int idx, int w,int h);
+
+LIBXSMM_API_INLINE unsigned int libxsmm_dnn_fill_sf_curve_index_map(unsigned char **sf_curve_index_map, unsigned int Mb, unsigned int Nb);
+
+LIBXSMM_API_INLINE void libxsmm_dnn_extract_indices_from_sf_curve(int *i_m, int *i_n, unsigned char *sf_curve_index_map, int sf_curve_index, unsigned int index_tsize);
+
+#endif
+
